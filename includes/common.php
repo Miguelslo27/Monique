5e4efc51553b337fafdd2b7e796af4fae24b8f49
Mike <miguel.sosa@globant.com>
2016-09-22 11:39:47 -0300

    Fix alias error for connection settings

--- includes/common.php
+++ includes/common.php
@@ -68,6 +68,7 @@ switch($_SERVER['HTTP_HOST']) {
 	break;
 	// PROD SERVER ENV
 	case 'monique.com.uy':
+	case 'www.monique.com.uy':
 		$db_dbase  = 'moniquec_db';
 		$dbaseHost = 'localhost';
 		$dbaseUser = 'moniquec_db_adm';
